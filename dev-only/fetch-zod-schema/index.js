6a453820606097f9d46c84586f0cb7df39871c75
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-02-10 16:14:42 +0800

    a

--- dev-only/fetch-zod-schema/index.js
+++ dev-only/fetch-zod-schema/index.js
@@ -5,7 +5,7 @@ const { curlCommand } = require('./postman/postman-data');
 const changeCase = require('change-case-all');
 
 function schemaFallback(name) {
-	return `
+  return `
 		lorem	// TODO: fake schema (generated), please check it
 		const ${name}Schema = z.object({
 			${changeCase.snakeCase(name)}_name: z.string().min(2, {
@@ -17,24 +17,31 @@ function schemaFallback(name) {
 			${changeCase.snakeCase(name)}_logo: z.string().optional(),
 			status: z.boolean(),
 		});
-	`
+	`;
 }
 
 module.exports = {
-	fetchZodSchema: (path, name) => {
-		const obj = endpoints.find(endpoint => endpoint.name === changeCase.camelCase(path));
+  fetchZodSchema: (path, name) => {
+    const obj = endpoints.find(
+      (endpoint) => endpoint.name === changeCase.camelCase(path),
+    );
 
-		try {
-			if(!obj) throw Error(`${path} doesn't exist, check endpoint`);
-			const response = execSync(curlCommand(obj.endpoint)).toString();
+    try {
+      if (!obj) throw Error(`${path} doesn't exist, check endpoint`);
+      const response = execSync(curlCommand(obj.endpoint)).toString();
 
-			const jsonResponse = JSON.parse(response);
-			if(!jsonResponse.data?.length) throw Error(`${obj.endpoint || path} endpoint returns nothing :(`);
+      const jsonResponse = JSON.parse(response);
+      if (!jsonResponse.data?.length)
+        throw Error(`${obj.endpoint || path} endpoint returns nothing :(`);
 
-			return jsonToZod(jsonResponse.data[0], changeCase.camelCase((name || obj.name) + "Schema"));
-		} catch (error) {
-			console.error(`Error:`, error.message);
-			return schemaFallback(name || path);
-		}
-	}
-}
+      const { id, created_at, updated_at, ...itemData } = jsonResponse.data[0];
+      return jsonToZod(
+        itemData,
+        changeCase.camelCase((name || obj.name) + 'Schema'),
+      );
+    } catch (error) {
+      console.error(`Error:`, error.message);
+      return schemaFallback(name || path);
+    }
+  },
+};
