cf4d7f0a0819a946c50e2d89940cb8fb1f7f6e53
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-02-10 19:18:50 +0800

    aksjdfoiwueirj

---
+++ dev-only/fetch-zod-schema/postman/generated-routes.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const dashboardDir = path.resolve(__dirname, '../../../app/(dashboard)');
+const generatedRoutes = fs
+  .readdirSync(dashboardDir)
+  .filter((file) => fs.statSync(path.join(dashboardDir, file)).isDirectory());
+
+module.exports = { generatedRoutes };
