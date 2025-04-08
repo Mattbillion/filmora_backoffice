a0e48841d8fea5b603500a9ac793d792d17008a3
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-04-08 20:59:09 +0800

    a

--- app/(builder)/templateDetail/[id]/client/stage.tsx
+++ app/(builder)/templateDetail/[id]/client/stage.tsx
@@ -1,9 +1,17 @@
 'use client';
 
-import { JSX, useEffect, useRef } from 'react';
-import { Layer, Stage as KonvaStage } from 'react-konva';
+import { JSX, useEffect, useRef, useState } from 'react';
+import { Group, Layer, Rect, Stage as KonvaStage } from 'react-konva';
 import Konva from 'konva';
 import { debounce } from 'lodash';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
 
 const scaleBy = 1.05;
 const maxScale = 10;
@@ -28,6 +36,9 @@ export default function Stage({
 }) {
   const stageRef = useRef<Konva.Stage>(null);
   const sectionsRef = useRef<Konva.Node[]>([]);
+  const [masks, setMasks] = useState<
+    { x: number; y: number; width: number; height: number }[]
+  >([]);
 
   useEffect(() => {
     if (stageRef.current) {
@@ -158,6 +169,27 @@ export default function Stage({
     return zoomInfo;
   };
 
+  const groupByFirstPart = () => {
+    const result: Record<string, Record<string, Konva.Node[]>> = {};
+
+    for (let i = 0; i < sectionsRef.current.length; i++) {
+      const node = sectionsRef.current[i];
+      const [firstPart, secondPart] = node.id().replace(/_/g, '-').split('-');
+
+      if (!result[firstPart]) result[firstPart] = {};
+
+      if (secondPart) {
+        if (!result[firstPart][secondPart]) result[firstPart][secondPart] = [];
+        result[firstPart][secondPart].push(node);
+      } else {
+        if (!result[firstPart]['Zone']) result[firstPart]['Zone'] = [];
+        result[firstPart]['Zone'].push(node);
+      }
+    }
+
+    return result;
+  };
+
   return (
     <div className="flex">
       <KonvaStage
@@ -193,27 +225,122 @@ export default function Stage({
         }}
         className="flex-1"
       >
-        <Layer>{shapes}</Layer>
+        <Layer>
+          {shapes}
+          <Group id="mask">
+            {masks.map((mask, idx) => (
+              <Rect
+                key={idx}
+                width={mask.width}
+                height={mask.height}
+                x={mask.x}
+                y={mask.y}
+                fill="#00D2FF"
+                stroke="black"
+                strokeWidth={2}
+              />
+            ))}
+          </Group>
+        </Layer>
       </KonvaStage>
-      <div className="flex-[462px] flex-col items-start border-l border-border">
+      <div className="flex h-[calc(100dvh-64px)] flex-[462px] flex-col border-l border-border">
         <h1 className="border-b p-4">Seatmap builder</h1>
-        <div>
-          {sectionsRef.current?.map((c, idx) => (
-            <button
-              key={idx}
-              className="w-full"
-              onClick={() => {
-                const node = stageRef.current?.findOne(
-                  (cc: Konva.Shape) =>
-                    cc.attrs['data-testid'] === c.attrs['data-testid'],
+        <div className="min-h-0 flex-1">
+          <div className="max-h-full space-y-4 overflow-y-auto p-4">
+            {Object.entries(groupByFirstPart())?.map(
+              ([label, seatmap], idx) => {
+                const stage = stageRef.current;
+                if (!stage) return null;
+
+                return (
+                  <Accordion type="single" key={idx} collapsible>
+                    <AccordionItem
+                      value={label}
+                      className="rounded-lg border px-4"
+                    >
+                      <AccordionTrigger>
+                        {label.replace(/^F+(\d+)/g, 'Floor: $1')}
+                      </AccordionTrigger>
+                      <AccordionContent className="space-y-2 [&>div:last-child>div]:border-b-0">
+                        {Object.entries(seatmap).map(
+                          ([sectionName, section], idx1) => {
+                            return (
+                              <Accordion type="multiple" key={idx1}>
+                                <AccordionItem
+                                  value={label}
+                                  className="px-4 [&[data-state=open]]:rounded-lg [&[data-state=open]]:bg-secondary"
+                                >
+                                  <AccordionTrigger className="pb-4 pt-2">
+                                    {sectionName.replace(/^Z+(.)/g, 'Zone: $1')}
+                                  </AccordionTrigger>
+                                  <AccordionContent>
+                                    {section.map((sector, idx2) => {
+                                      const node = stage.findOne(
+                                        (cc: Konva.Shape) =>
+                                          cc.attrs['data-testid'] ===
+                                          sector.attrs['data-testid'],
+                                      );
+                                      // @ts-ignore
+                                      let sectorName = sector
+                                        .id()
+                                        .split(/[\s-]+/)
+                                        .at(-1)
+                                        .replace('_', ' ');
+
+                                      if (/(\S+-S(\d+))$/g.test(sector.id()))
+                                        sectorName = sector
+                                          .id()
+                                          .replace(
+                                            /(\S+-S(\d+))$/g,
+                                            'Section: $2',
+                                          );
+
+                                      return (
+                                        <button
+                                          key={idx2}
+                                          className="w-full"
+                                          disabled={!node}
+                                          onClick={() => {
+                                            if (node) {
+                                              const {
+                                                x,
+                                                y,
+                                                width: nodeW,
+                                                height: nodeH,
+                                              } = node.getClientRect({
+                                                relativeTo: stage,
+                                              });
+                                              setMasks([
+                                                ...masks,
+                                                {
+                                                  x,
+                                                  y,
+                                                  width: nodeW,
+                                                  height: nodeH,
+                                                },
+                                              ]);
+                                            }
+                                          }}
+                                        >
+                                          {sectorName}
+                                        </button>
+                                      );
+                                    })}
+                                  </AccordionContent>
+                                </AccordionItem>
+                              </Accordion>
+                            );
+                          },
+                        )}
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
                 );
-                if (node) node.remove();
-              }}
-            >
-              {c.id()}
-            </button>
-          ))}
+              },
+            )}
+          </div>
         </div>
+        <Button className="m-4">Build</Button>
       </div>
     </div>
   );
