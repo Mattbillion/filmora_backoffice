acd262e7160befec33b854b86af23a42674ebe19
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-04-09 19:46:26 +0800

    a

--- app/(dashboard)/merchandises/layout.tsx
+++ app/(dashboard)/merchandises/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import { notFound } from 'next/navigation';
 
+// import { notFound } from 'next/navigation';
 import { auth } from '@/app/(auth)/auth';
 import { checkPermission } from '@/lib/permission';
 
@@ -8,5 +8,6 @@ export default async function Layout({ children }: { children: ReactNode }) {
   const session = await auth();
 
   if (checkPermission(session, ['company_merchandise_list'])) return children;
-  return notFound();
+  // return notFound();
+  return children; // TODO: tvr zuur
 }
