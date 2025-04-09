2e55cee2a44a475ef32e5035939d4c223c72fee4
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-04-09 14:19:31 +0800

    a

--- app/(dashboard)/role/components/update-dialog.tsx
+++ app/(dashboard)/role/components/update-dialog.tsx
@@ -6,8 +6,6 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
 
 import FormDialog, { FormDialogRef } from '@/components/custom/form-dialog';
-import HtmlTipTapItem from '@/components/custom/html-tiptap-item';
-import UploadImageItem from '@/components/custom/upload-image-item';
 import {
   FormControl,
   FormField,
@@ -85,13 +83,6 @@ export function UpdateDialog({
       />
       <FormField
         control={form.control}
-        name="role_logo"
-        render={({ field }) => (
-          <UploadImageItem field={field} imagePrefix="role_logo" label="Logo" />
-        )}
-      />
-      <FormField
-        control={form.control}
         name="status"
         render={({ field }) => (
           <FormItem>
@@ -113,11 +104,6 @@ export function UpdateDialog({
             <FormMessage />
           </FormItem>
         )}
-      />
-      <FormField
-        control={form.control}
-        name="role_desc"
-        render={({ field }) => <HtmlTipTapItem field={field} />}
       />
     </FormDialog>
   );
