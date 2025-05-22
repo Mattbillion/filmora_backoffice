37117749a075d33789e1516fa3a0c6358dc0e52a
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-05-22 17:19:51 +0800

    a

--- lib/fetch/index.ts
+++ lib/fetch/index.ts
@@ -55,8 +55,10 @@ export async function xooxFetch<
         headers.set('Authorization', `Bearer ${session?.user?.id}`);
       if (!opts.searchParams) opts.searchParams = {};
 
-      if (opts.method === 'GET')
+      if (opts.method === 'GET') {
         opts.searchParams.company_id = session?.user?.company_id;
+        opts.searchParams.com_id = session?.user?.company_id;
+      }
     }
 
     const { endpoint, fetchOptions } = genFetchParams(url, {
