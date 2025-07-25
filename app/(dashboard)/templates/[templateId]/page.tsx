3260fb855369ddf68c5f5d9f57ed9322c4ae24d1
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-07-25 15:06:31 +0800

    a

--- app/(dashboard)/templates/[templateId]/page.tsx
+++ app/(dashboard)/templates/[templateId]/page.tsx
@@ -70,7 +70,7 @@ export default async function TemplateDetailPage(props: {
           },
         }}
       />
-      <Heading title="Template schedules" />
+      <Heading title="Template" />
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
         <Card>
           <CardHeader>
