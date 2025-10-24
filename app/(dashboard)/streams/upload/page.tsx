1bbd79245ba941d740bc1b0fb861571e15cfb2ff
mattbillion <muugiiyep0722@gmail.com>
2025-10-24 19:46:44 +0800

    changes.

--- app/(dashboard)/streams/upload/page.tsx
+++ app/(dashboard)/streams/upload/page.tsx
@@ -1,5 +1,22 @@
-import UppyUpload from '@/app/(dashboard)/streams/uppy';
+'use client';
+import React from 'react';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+import { UppyUpload } from '@/app/(dashboard)/streams/uppy';
+import { Button } from '@/components/ui/button';
 
 export default function Page() {
-  return <UppyUpload />;
+  const router = useRouter();
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-2">
+        <Button variant="outline" size="icon" onClick={() => router.back()}>
+          <ArrowLeft />
+        </Button>
+        <h1 className="text-xl font-medium">Кино оруулах</h1>
+      </div>
+      <UppyUpload />
+    </div>
+  );
 }
